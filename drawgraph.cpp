cbf3e78d16ac309edfd5237c62d4e91447026698
grevutiu-gabriel <gabriel1984sibiu@gmail.com>
2013-11-24 12:16:59 +0200

    Change drawgraph.cpp

--- drawgraph.cpp
+++ drawgraph.cpp
@@ -10,9 +10,10 @@
 int main(int nNumberofArgs, char *pszArgs[]) {
   const char *filename;
   filename=pszArgs[1];
-  unsigned float xd,yd,rezultat;
+  double xd,yd;
   double xshow,yshow;
-  float numar;
+  double numarx, numary;
+  double rezultat;
   
   png::image< png::rgb_pixel > image(filename);
   std::cout<<"inaltimea imagini: "<<image.get_height()<<"\n";
@@ -20,27 +21,35 @@ int main(int nNumberofArgs, char *pszArgs[]) {
   //for (size_t y = 0; y < image.get_height(); ++y) {
     //for (size_t x = 0; x < image.get_width(); ++x) {
   for (size_t y = 0; y < image.get_height(); ++y) {
+	numary=0;	
+	for (int i=0;i<5;++i) {	
+	yd=numary+y;
+	yshow=round(yd);
+	}
     for (size_t x = 0; x < image.get_width(); ++x) {
-	numar=0;
+	numarx=0;
 	for (int i=0; i<5; ++i) {
-		numar+=0.2;
-		xd=numar+x;
-		yd=numar+y;
+		numarx+=0.2;
+		xd=numarx+x;
 		xshow=round(xd);
-		yshow=round(yd);
+		
 		//std::cout<<"Valoare lui x este: "<<xshow<<" Valoare lui y este: "<<yshow<<"\n";
 		//std::cout<<"x : "<<xshow<<" y : "<<yshow<<"\n";
 		}
   //numar+=1;
   //if(((x*x*x-y)%60) <= 35) image[y][x] = png::rgb_pixel(0, numar, numar);
-  //if(((x+y)%60) <= x+y) image[y][x] = png::rgb_pixel(0, 0, 0);
+  if(((x+y)%60) <= x+y) image[y][x] = png::rgb_pixel(0, 0, 0);
   //if (((x+y)%60) <= 35) image[y][x] = png::rgb_pixel(0, 255, 0);
   //if (yshow==round(xd*xd)) {image[yshow][xshow] = png::rgb_pixel(0, 255, 0); std::cout<<"egal"<<"\n";}
   
   //else std::cout<<"neegal"<<"\n";
-    }
   rezultat=xd*xd;
-  std::cout<<rezultat<<"\n";
+  printf("valoare xd este %.2lf\n", xd);
+  printf("round of  %4.2lf is  %.2lf\n", rezultat, round(rezultat));
+    }
+  
+  
+  //std::cout<<rezultat<<"\n";
   if (yshow==rezultat) image[yshow][xshow] = png::rgb_pixel(0, 255, 0);
   }
  
