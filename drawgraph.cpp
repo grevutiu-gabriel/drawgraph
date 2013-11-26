f8fd697834a98023c45bca89db1abe4490b29698
grevutiu-gabriel <gabriel1984sibiu@gmail.com>
2013-11-26 20:51:40 +0200

    Improve algorithm for increase speed

--- drawgraph.cpp
+++ drawgraph.cpp
@@ -11,7 +11,7 @@ int main(int nNumberofArgs, char *pszArgs[]) {
   const char *filename;
   filename=pszArgs[1];
   double xd,yd;
-  double xshow,yshow;
+  double xshow,yshow,xplot,yplot;
   double numarx, numary;
   double rezultat, rotunjit;
   int xvar=0;
@@ -21,12 +21,12 @@ int main(int nNumberofArgs, char *pszArgs[]) {
   cout<<"lungimea imagini: "<<image.get_width()<<"\n";
   //for (size_t y = 0; y < image.get_height(); ++y) {
     //for (size_t x = 0; x < image.get_width(); ++x) {
-  for (size_t y = 0; y < image.get_height(); ++y) {
+  /*for (size_t y = 0; y < image.get_height(); ++y) {
 	numary=0;	
 	for (int i=0;i<50;++i) {
 	numary+=0.02;
 	yd=numary+y;
-	yshow=round(yd);
+	yshow=round(yd);*/
 	
 	
     for (size_t x = xvar; x < image.get_width(); ++x) {
@@ -50,21 +50,31 @@ int main(int nNumberofArgs, char *pszArgs[]) {
   //printf("round of  %4.2lf is  %.2lf\n", rezultat, round(rezultat));
   //cout<<rezultat<<"\n";
   
-  if (sqrt(yd)-1>xd) xvar=round(sqrt(yd))-1;
-  if (sqrt(yd)<(xd-1) || round(xd*xd)>image.get_height()+1) break; //limiteaza valoarea produsului xd*xd la valoarea inaltimii imaginii 
+  //if (sqrt(yd)-1>xd) xvar=round(sqrt(yd))-1;
+  //if (sqrt(yd)<(xd-1) || round(xd*xd)>image.get_height()+1) break; //limiteaza valoarea produsului xd*xd la valoarea inaltimii imaginii 
   //if (sqrt(yd)<round(xd) || round(xd*xd)>=image.get_height()) break; //limiteaza valoarea produsului xd*xd la valoarea inaltimii imaginii
   //if (round(xd*xd)>=image.get_height()) break; //limiteaza valoarea produsului xd*xd la valoarea inaltimii imaginii 
   //if (yshow==rotunjit) image[(unsigned long)yshow][(unsigned long)xshow] = png::rgb_pixel(0, 255, 0);
-  if (yshow==rotunjit && yshow < image.get_height()) {image[yshow][xshow] = png::rgb_pixel(0, 255, 0); printf("valoarea xd este %.2lf; valoarea xd patrat este %.2lf; valoarea rotunjita xd patrat %.2lf; valoare yd %.2lf; egal\n", xd, rezultat, rotunjit, round(yd));}
-  else printf("valoarea xd este %.2lf; valoarea xd patrat este %.2lf; valoarea rotunjita xd patrat %.2lf; valoare yd %.2lf; diferit\n", xd, rezultat, rotunjit, round(yd));
+  //if (rotunjit>image.get_height()) break; //limiteaza valoarea produsului xd*xd la valoarea inaltimii imaginii
+  xplot=xshow;
+  yplot=rotunjit;
+  //if (yplot < image.get_height()) image[yplot][xplot] = png::rgb_pixel(0, 255, 0);
+  //else break;
+  if (yplot < image.get_height())
+{image[yplot][xplot] = png::rgb_pixel(0, 255, 0); printf("valoarea xd este %.2lf; valoarea xd patrat este %.2lf; valoarea rotunjita xd patrat %.2lf; valoare yd %.2lf\n", xshow, rezultat, rotunjit, round(yd));}
+  else break;
+ 
+ //if (yshow==rotunjit && yplot < image.get_height())
+//{image[yplot][xplot] = png::rgb_pixel(0, 255, 0); printf("valoarea xd este %.2lf; valoarea xd patrat este %.2lf; valoarea rotunjita xd patrat %.2lf; valoare yd %.2lf; egal\n", xshow, rezultat, rotunjit, round(yd));}
+  //else printf("valoarea xd este %.2lf; valoarea xd patrat este %.2lf; valoarea rotunjita xd patrat %.2lf; valoare yd %.2lf; diferit\n", xshow, rezultat, rotunjit, round(yd));
   
   /*if (yshow==round(xd*xd)) {image[yshow][xshow] = png::rgb_pixel(0, 255, 0); cout<<"egal"<<"\n";}
   else cout<<"diferit"<<"\n";*/
 		}
     }
-   }
+  // }
     
-  }
+  //}
  
  image.write(filename);
 }
